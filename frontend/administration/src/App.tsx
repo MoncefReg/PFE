be3b7b071fecf5a2d1578902ab67269fa8b4b844
MoncefReg <m.reggam@esi-sba.dz>
2022-04-23 18:19:06 +0100

    PFE-7 added Mui theme

--- frontend/administration/src/App.tsx
+++ frontend/administration/src/App.tsx
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import './i18n/config';
 
+// Theme
+import { ThemeProvider } from '@emotion/react';
+import createTheme from './theme';
+
 function App() {
+  const theme = createTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/administration/src/theme/typography.ts
@@ -0,0 +1,56 @@
+const typography = {
+  fontFamily: 'Noto Sans HK',
+  h1: {
+    fontWeight: 500,
+    fontSize: 42,
+    lineHeight: '60px'
+  },
+  h2: {
+    fontWeight: 500,
+    fontSize: 29
+  },
+  h3: {
+    fontWeight: 500,
+    fontSize: 24
+  },
+  h4: {
+    fontWeight: 500,
+    fontSize: 20
+  },
+  h5: {
+    fontWeight: 400,
+    fontSize: 17,
+    lineHeight: '22px'
+  },
+  h6: {
+    fontWeight: 400,
+    fontSize: 15,
+    lineHeight: '20px'
+  },
+  body1: {
+    fontWeight: 500,
+    fontSize: 17,
+    lineHeight: '22px',
+    fontFamily: 'Noto Sans HK'
+  },
+  body2: {
+    fontWeight: 500,
+    fontSize: 15,
+    lineHeight: '20px',
+    fontFamily: 'Noto Sans HK'
+  },
+  subtitle1: {
+    fontWeight: 300,
+    fontSize: 13,
+    lineHeight: '11px',
+    fontFamily: 'Noto Sans HK'
+  },
+  subtitle2: {
+    fontWeight: 300,
+    fontSize: 12,
+    lineHeight: '12px',
+    fontFamily: 'Noto Sans HK'
+  }
+};
+
+export default typography;
