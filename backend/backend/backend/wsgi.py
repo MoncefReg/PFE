815e6b66a4b1802bd221e0d23cdc6b43850100c9
MoncefReg <m.reggam@esi-sba.dz>
2022-04-24 18:52:27 +0100

    PFE-19 adding django backend project

---
+++ backend/backend/backend/wsgi.py
@@ -0,0 +1,15 @@
+import os
+import dotenv
+
+from django.core.wsgi import get_wsgi_application
+
+dotenv.load_dotenv(
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.development')
+
+if os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = os.getenv('DJANGO_SETTINGS_MODULE')
+
+application = get_wsgi_application()
