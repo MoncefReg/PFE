4d54dfacc4b6f8087f954f375dbaaa4425d3da1f
MoncefReg <m.r@swissdigilab.com>
2022-05-15 22:58:51 +0100

    Adding dropzone

---
+++ frontend/administration/src/components/useDropzone/Dropzone.tsx
@@ -0,0 +1,163 @@
+import React, { useState, useCallback } from 'react';
+
+// Hooks
+import { useTranslation } from 'react-i18next';
+
+// UI
+import {
+  Box,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Tooltip,
+  Typography
+} from '@mui/material';
+import { FileCopy, HighlightOff } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+
+// Others
+import { useDropzone } from 'react-dropzone';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import clsx from 'clsx';
+
+const PREFIX = 'DROPZONE';
+
+const classes = {
+  dropZone: `${PREFIX}-root`,
+  dragActive: `${PREFIX}-drag-active`
+};
+
+const DropZoneWrapper = styled('div')(({ theme }) => ({
+  [`&.${classes.dropZone}`]: {
+    border: `1px dashed ${theme.palette.divider}`,
+    padding: theme.spacing(6),
+    outline: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    transition: '400ms background-color',
+    '&:hover': {
+      backgroundColor: theme.palette.action.hover,
+      opacity: 0.5,
+      cursor: 'pointer'
+    }
+  },
+  [`&.${classes.dragActive}`]: {
+    backgroundColor: theme.palette.action.active,
+    opacity: 0.5
+  }
+}));
+
+const StyledButtonWrapper = styled('div')``;
+
+const DropZone = (maxFiles: number, accept: any) => {
+  const [files, setFiles] = useState<any[]>([]);
+
+  const { t } = useTranslation();
+  const handleDrop = useCallback(
+    (acceptedFiles: any) => {
+      if (files.length < maxFiles)
+        setFiles((prevFiles) => [...prevFiles].concat(acceptedFiles));
+    },
+    [maxFiles, files]
+  );
+
+  const handleRemoveAll = () => {
+    setFiles([]);
+  };
+
+  const handleRemove = (key: any) => {
+    if (files.length === 1) setFiles([]);
+    setFiles(files.filter((elm, index) => index !== key));
+  };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: handleDrop,
+    accept,
+    maxFiles
+  });
+
+  const getImage = (url: string) => {
+    const image: any = new Image();
+    image.src = url;
+    image.size = image.width * image.height;
+    image.name = url.split('/')[url.split('/').length - 1];
+    return image;
+  };
+
+  const markup = (
+    <div>
+      <DropZoneWrapper
+        className={clsx(classes.dropZone, {
+          'DROPZONE-drag-active': isDragActive
+        })}
+        {...getRootProps()}
+      >
+        <input {...getInputProps()} hidden />
+        <div>
+          <img alt="..." width={130} src="/static/dropzone-files-icon.svg" />
+        </div>
+        <div>
+          <Typography gutterBottom variant="h3">
+            {t('SELECT_FILES')}
+          </Typography>
+          <Box mt={2}>
+            <Typography color="textPrimary" variant="body1">
+              {t('DROP_OR_CLICK_MESSAGE')}
+            </Typography>
+          </Box>
+        </div>
+      </DropZoneWrapper>
+      {files.length > 0 && (
+        <>
+          <PerfectScrollbar options={{ suppressScrollX: true }}>
+            <List
+              sx={{
+                maxHeight: 320
+              }}
+            >
+              {files.map((file, i) => (
+                <ListItem divider={i < files.length - 1} key={i}>
+                  <ListItemIcon>
+                    <FileCopy />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={file.name ? file.name : getImage(file).name}
+                    primaryTypographyProps={{ variant: 'h5' }}
+                  />
+                  <Tooltip title={t('DELETE')}>
+                    <IconButton edge="end" onClick={() => handleRemove(i)}>
+                      <HighlightOff />
+                    </IconButton>
+                  </Tooltip>
+                </ListItem>
+              ))}
+            </List>
+          </PerfectScrollbar>
+          <StyledButtonWrapper
+            sx={{
+              mt: 2,
+              display: 'flex',
+              justifyContent: 'flex-end',
+              '& > * + *': {
+                ml: 2
+              }
+            }}
+          >
+            <Button onClick={handleRemoveAll} variant="contained">
+              {t('REMOVE_ALL')}
+            </Button>
+          </StyledButtonWrapper>
+        </>
+      )}
+    </div>
+  );
+
+  return [markup, files, setFiles];
+};
+
+export default DropZone;
---
+++ frontend/administration/src/components/useDropzone/index.tsx
@@ -0,0 +1,39 @@
+// UI
+import { Box, Grid, Paper } from '@mui/material';
+
+// Others
+import FilesDropzone from './Dropzone';
+import FileCard from './File';
+
+const Files = ({
+  maxFiles = 1,
+  accept = 'image/jpeg, image/png',
+  showCards = false
+}) => {
+  const [FilesZone, files, setFiles] = FilesDropzone(maxFiles, accept);
+
+  const markup = (
+    <div>
+      <Paper sx={{ p: 2 }} elevation={24}>
+        <>
+          {FilesZone}
+          {showCards && (
+            <Box mt={3}>
+              <Grid container spacing={3}>
+                {(files as any[]).map((file) => (
+                  <Grid item key={file.id} lg={4} md={4} sm={6} xs={12}>
+                    <FileCard file={file} />
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          )}
+        </>
+      </Paper>
+    </div>
+  );
+
+  return [markup, files, setFiles];
+};
+
+export default Files;
